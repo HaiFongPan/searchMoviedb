8214df7fd07e5e8706e6f4a2f0e76a66be19a1a0
HaiFongPan <leopan.me@gmail.com>
2014-01-20 15:00:07 +0800

    Update func.js

--- func.js
+++ func.js
@@ -58,7 +58,7 @@ function GetMovieById(id){
                 if(i!=0)
                     li += '/'
             };
-        　　  li = li + '</li></ul></div><div id="Summary"><li class ="summary">'+json.summary+'</li></div>'
+        　  li = li + '</li></ul></div><div id="Summary"><li class ="summary">'+json.summary+'</li></div>'
             console.log(li)
             buildDomTree(li)
         }
